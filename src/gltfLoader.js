0bdf59db37b22bb4b8e4294826ef01a955b7016c
arjablc <arjablc@gmail.com>
2023-10-07 14:15:35 +0545

    asset loader error fixed, model added

--- src/gltfLoader.js
+++ src/gltfLoader.js
@@ -2,7 +2,7 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 export const loadAsset = async (path) => {
 	const loader = new GLTFLoader().setPath("../public");
-	let gltf = await loader.loadAsset(path);
+	let gltf = await loader.load(path);
 	let model = gltf.scene.children[0];
 	model.scale.set(0.01, 0.01, 0.01);
 	return model;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import "../style.css";
 import { Setup } from "./three_setup";
 import { loadAsset } from "./gltfLoader";
+import { TetrahedronGeometry } from "three";
 
 let element = document.querySelector(".render-container");
-
 let setup = new Setup(element);
+let moon = await loadAsset("./moon");
+const pLight = new THREE.PointLight(0xffffff, 10000);
+pLight.position.set(0, 50, 0);
+
+setup.scene.add(moon);
+
 const animate = () => {
 	window.requestAnimationFrame(animate);
 	setup.update();
