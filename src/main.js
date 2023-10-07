e51837d7df7ecbd1b68155722d52d47e42f4eecd
Anuj Paudel <anujpaudel98@yahoo.com>
2023-10-07 17:23:14 +0545

    sun colour in ecclipse

--- src/main.js
+++ src/main.js
@@ -5,11 +5,23 @@ import * as THREE from "three";
 import { gsap } from "gsap";
 //initial camera position
 let setup = new Setup();
-setup.camera.position.z = 30;
+setup.camera.position.z = 100;
 
+//background stars
+const loader = new THREE.CubeTextureLoader();
+const texture = loader.load([
+    '../public/2k_stars.jpg',
+    '../public/2k_stars.jpg',
+    '../public/2k_stars.jpg',
+    '../public/2k_stars.jpg',
+    '../public/2k_stars.jpg',
+    '../public/2k_stars.jpg'
+]);
+
+setup.scene.background = texture;
 
 //sun model
-const sunGeometry = new THREE.SphereGeometry(1, 32, 32); // Radius, widthSegments, heightSegments
+const sunGeometry = new THREE.SphereGeometry(0.5, 32, 32); // Radius, widthSegments, heightSegments
 const sunMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 }); 
 const sun = new THREE.Mesh(sunGeometry, sunMaterial);
 setup.scene.add(sun);
@@ -21,11 +33,11 @@ textureLoader.load('../public/2k_sun.jpg', (texture) => {
 });
 
 //to change color of sun
-sun.material.color.set(0.7,0.2,0)
+// sun.material.color.set(0.7,0.2,0)
+
 
 //loading moon and sun
 let moon = await loadAsset("../public/moon.glb");
-let sun = await loadAsset("../public/sun_model.glb");
 const sunPLight = new THREE.PointLight(0xffffff, 10000);
 sun.add(sunPLight);
 //setting moon and sun scale and position
@@ -35,8 +47,6 @@ moon.position.set(50, 0, 50);
 const maxSunScale = 20;
 const maxMoonScale = 0.009;
 const maxMoonDisp = 0;
-//ambient Light
-const ambLight = new THREE.AmbientLight(0xffffff, 1);
 
 const gridH = new THREE.GridHelper(200, 10);
 setup.scene.add(gridH, sun);
@@ -52,9 +62,16 @@ function scaleIn() {
 	});
 }
 
+const axesHelper = new THREE.AxesHelper( 500 );
+setup.scene.add( axesHelper );
+
 function animateEclipse() {
 	moon.rotation.y += 0.01;
-	if (moon.position.x > maxMoonDisp) moon.position.x -= 0.1;
+	if(0.1<moon.position.x && moon.position.x<15)
+	{
+		sun.material.color.g-=0.004;
+	}
+	if (moon.position.x > maxMoonDisp) moon.position.x -= 0.1;	
 }
 
 function animate() {
